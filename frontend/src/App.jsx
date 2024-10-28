fdea7f20b456a804a1dd4d79b62b220e7c8c65d1
Aman Dhingra <amandhingra2336@gmail.com>
2024-10-28 16:44:03 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import Login from "./pages/Login.jsx";
 import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
