a21fb31fbd13ac03ae0c8977d593a446afcdf8f8
Aman Dhingra <amandhingra2336@gmail.com>
2024-11-01 00:52:08 +0530

    spotify api init

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 import UserRouter from "./routes/UserRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import cors from "cors";
